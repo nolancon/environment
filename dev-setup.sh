28821447d9f9789e3a06443b0055f1e87aec602e
nolancon <conor.nolan@intel.com>
2021-04-05 11:11:04 +0000

    add vim update

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-update_git()
+yum_deps()
 {
-  git version
-  if [ $? -eq 0 ]; then
-     echo OK
-  else
-     echo "git not present, installing git..."
-     yum install  -y git
-  fi
+  yum update -y
+  yum install -y git wget make gcc glibc-devel dnf-plugins-core yum-utils 	
+  yum config-manager  --set-enabled PowerTools
+}
+
+git_config()
+{
   git config --global user.name "nolancon"
   git config --global user.email conor.nolan@intel.com
 }  
 
-setup_go()
+go_setup()
 {	
-  yum install -y wget
   wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
   tar -C /usr/local -xvzf go1.15.2.linux-amd64.tar.gz
   mkdir -p /root/go_projects/{bin,src,pkg}
@@ -26,34 +25,42 @@ setup_go()
   if [ $? eq 0 ]; then
      echo "failed to setup go"
   fi
-} 
+}
+
+vim_update()
+{
+  cd $GOPATH/src
+  mkdir -p github.com/vim
+  cd github.com/vim
+  git clone --branch v8.1.2269 https://github.com/vim/vim.git
+  yum-builddep vim 
+  cd vim
+  ./configure
+  make
+  make install
+  yes | cp -f /usr/bin/vi /root/old_vi
+  yes | cp -f /usr/local/bin/vim /usr/bin/vi 
+  cd ~
+}
 
 go_vim()
 {
   rm -rf ~/.vim/bundle/Vundle.vim
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#  rm -rf ~/.vimrc
+  rm -rf ~/.vimrc
   wget -c https://raw.githubusercontent.com/nolancon/environment/master/vimrc -O ~/.vimrc
-  echo RUNVI
   vi +PluginUpdate +PluginInstall +qall
 }
 
-update_vim()
+bashrc_update()
 {
-  yum install -y make gcc glibc-devel	 
-  cd $GOPATH/src/github.com
-#  mkdir vim && cd vim
-#  git clone --branch v8.1.2269 https://github.com/vim/vim.git
-  cd vim/vim
-  #export CFLAGS="-DCOMPILER_FLAG ./configure --enable-gui=motif"
-  ./configure CFLAGS="-O3"
-  echo CONFIG
-  make
-  make install
-  cd /
+  wget -c https://raw.githubusercontent.com/nolancon/environment/master/bashrc -O ~/.bashrc
+  source ~/.bashrc
 }
 
-#update_git 
-#setup_go
-update_vim
-#go_vim
+yum_deps
+git_config
+go_setup
+vim_update
+go_vim
+bashrc_update
