f598c3e9563cbb7022be62d8c665d1587bd48337
nolancon <conor.nolan@ondat.io>
2022-02-16 12:06:16 +0000

    Add vim yaml settings

--- scripts/vim-load.sh
+++ scripts/vim-load.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/vendor/start/indentLine
+vi -u NONE -c "helptags  ~/.vim/pack/vendor/start/indentLine/doc" -c "q"
+
 vi +PluginUpdate +PluginInstall +GoInstallBinaries +qall
