f73e565f31e45d9efae2e6fe2e6bedc88396ecb9
nolancon <conor.nolan@ondat.io>
2023-03-09 14:21:00 +0000

    fix vim commands

--- scripts/vim-load.sh
+++ scripts/vim-load.sh
@@ -3,4 +3,4 @@
 git clone https://github.com/Yggdroot/indentLine.git ~/.vim/pack/vendor/start/indentLine
 vi -u NONE -c "helptags  ~/.vim/pack/vendor/start/indentLine/doc" -c "q"
 
-vi +PluginUpdate +PluginInstall +GoInstallBinaries +qall
+vi +PlugUpdate +PlugInstall +GoInstallBinaries +qall
